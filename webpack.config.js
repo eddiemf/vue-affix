e07e7feac06cb4abed0546f473ee531bcfd39be8
Mauricio Farias Dziedzinski <d.mauriciofarias@gmail.com>
2019-04-08 00:30:20 +0200

    Make demo available in main repo (#38)

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    'vue-affix': path.resolve(__dirname, 'src/vue-affix.js'),
+    demo: path.resolve(__dirname, 'src/demo/demo.js'),
+  },
   output: {
-    filename: 'vue-affix.min.js',
-    path: path.resolve(__dirname, './dist'),
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
     library: {
       root: 'vueAffix',
@@ -14,19 +18,30 @@ module.exports = {
     },
     libraryTarget: 'umd',
   },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js',
+    },
+  },
+  devServer: {
+    contentBase: './src/demo',
+    stats: 'errors-only',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: ['babel-loader'],
       },
       {
         test: /\.vue$/,
-        use: ['vue-loader', 'eslint-loader'],
+        exclude: /node_modules/,
+        use: ['vue-loader'],
       },
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: ['vue-style-loader', 'css-loader'],
       },
     ],
